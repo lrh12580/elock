4dfd1f198556187bf54986ebd8e90cbfb7a34168
lrh12580 <lrh_12580@126.com>
2017-11-06 13:05:10 +0800

    Add files via upload

---
+++ Server/Results.java
@@ -0,0 +1,23 @@
+/**
+ * Created by admin on 2017/3/1.
+ */
+public class Results {
+    private boolean isPermit = false;
+    private boolean isCheck = false;
+
+    public boolean isPermit() {
+        return isPermit;
+    }
+
+    public void setPermit(boolean permit) {
+        isPermit = permit;
+    }
+
+    public boolean isCheck() {
+        return isCheck;
+    }
+
+    public void setCheck(boolean check) {
+        isCheck = check;
+    }
+}
